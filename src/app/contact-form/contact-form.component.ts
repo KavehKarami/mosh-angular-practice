3c1326853baeef6565b8c63ee00153cf6471a8a1
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2020-05-13 23:00:36 +0430

    end of Template-driven Forms

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  constructor() {}
 
-  constructor() { }
+  onChange = (target) => {
+    console.log(target);
+  };
 
-  ngOnInit(): void {
+  onSubmit(f) {
+    console.log(f.value.firstName);
+    alert(`your First Name is: ${f.value.firstName}`);
   }
-
 }
