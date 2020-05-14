15386325071bf14922bbc2cae26c192b4c9ecec5
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2020-05-14 23:34:59 +0430

    Http Service - CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SummaryPipe } from './summary.pipe';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +17,15 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     ContactFormComponent,
     SignupFormComponent,
     NewCourseFormComponent,
+    PostsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css'],
+})
+export class PostsComponent {
+  posts;
+
+  private url = 'http://localhost:8080/posts';
+
+  constructor(private http: HttpClient) {
+    this.getPosts();
+  }
+
+  getPosts() {
+    this.http.get(this.url).subscribe((response) => {
+      this.posts = response;
+    });
+  }
+
+  onDelete(post) {
+    this.http.delete(`${this.url}/${post.id}`).subscribe((response) => {
+      this.getPosts();
+    });
+  }
+
+  onPost(element: HTMLInputElement) {
+    const description = element.value;
+    element.value = '';
+    this.http.post(this.url, { description }).subscribe((response) => {
+      this.getPosts();
+    });
+  }
+
+  onPut(post, input: HTMLInputElement) {
+    const description = input.value;
+    input.value = '';
+    this.http
+      .patch(`${this.url}/${post.id}`, {
+        description: description,
+      })
+      .subscribe((response) => {
+        this.getPosts();
+      });
+  }
+}
