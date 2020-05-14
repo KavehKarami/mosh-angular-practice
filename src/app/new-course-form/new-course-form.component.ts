0cf0e3fceafd763bddade01f489ed5c7c6eede43
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2020-05-14 22:10:10 +0430

    end of Reactive Forms

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
@@ -7,10 +7,20 @@ import { FormArray, FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./new-course-form.component.css'],
 })
 export class NewCourseFormComponent {
-  form = new FormGroup({
-    topics: new FormArray([]),
-  });
+  // way 1 to create form
 
+  // form = new FormGroup({
+  //   topics: new FormArray([]),
+  // });
+
+  // second way
+
+  form;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      topics: fb.array([]),
+    });
+  }
   addTopic(topic: HTMLInputElement) {
     this.topics.push(new FormControl(topic.value));
 
