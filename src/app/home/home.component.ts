5a6a03ead7a5db03605ab9770bcd2bf552ef3302
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2020-05-16 22:16:23 +0430

    end of routing and navigate

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(protected router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    this.router.navigate(['/httpservice'], {
+      queryParams: { page: 1, order: 'newest' },
+    });
   }
-
 }
